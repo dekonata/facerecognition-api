7919df351924003e773febdcac7f362c4723fcdf
Kriegler Van der Merwe <kriegler@albsa.local>
2021-07-16 11:18:01 +0200

    server database config

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
   	connection: {
-    host : 'postgresql-cylindrical-86053',
-    user : 'postgres',
-    password : 'Ludium99',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
